cd70ee60f1a128f744d52609384acef3befd0dc7
Petar <pmatisic@foi.hr>
2022-12-03 12:35:52 +0100

    Inicijalni commit - dodan blank Spring projekt - namještene konfiguracije za početak rada

---
+++ Backend/ednevnik/src/test/java/org/ednevnik/EdnevnikApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ednevnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdnevnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
