4f60f8d58e3e123ab97b7dcb2af0454cdf88992d
Flaster99 <mmihaljevic.vz@gmail.com>
2022-12-04 15:29:19 +0100

    Kreiran xml za RecyclerView i klasa za specijalizanta

---
+++ Frontend/app/src/main/java/hr/foi/air/ednevnik/Specijalizant.kt
@@ -0,0 +1,9 @@
+package hr.foi.air.ednevnik
+
+class Specijalizant (
+    var id : Int? = null,
+    var ime : String = "",
+    var prezime : String = "",
+    var email : String = "",
+        ) {
+}
