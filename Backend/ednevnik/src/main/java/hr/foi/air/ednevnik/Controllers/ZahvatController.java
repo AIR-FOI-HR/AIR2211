f84564af6b45536edcb096cb6b4b59bc4f5c7a07
Roko <rokomilosevic@hotmail.com>
2023-01-08 17:24:14 +0100

    Dodana funkcija za dohvaćanje svih zahvata pojedinog programa specijalizacije.

---
+++ Backend/ednevnik/src/main/java/hr/foi/air/ednevnik/Controllers/ZahvatController.java
@@ -0,0 +1,35 @@
+package hr.foi.air.ednevnik.Controllers;
+
+import hr.foi.air.ednevnik.Entities.StrucniRad;
+import hr.foi.air.ednevnik.Entities.Zahvat;
+import hr.foi.air.ednevnik.Services.StrucniRadService;
+import hr.foi.air.ednevnik.Services.ZahvatService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin
+@RequestMapping("/zahvati")
+public class ZahvatController {
+
+    private ZahvatService zahvatService;
+
+    @GetMapping("/getAllByProgramSpecijalizacijeId/{program_specijalizacije_id}")
+    public ResponseEntity<List<Zahvat>> GetZahvatiByProgramSpecijalizacije(@PathVariable int program_specijalizacije_id){
+        var zahvati = zahvatService.ZahvatiByProgramSpecijalizacije(program_specijalizacije_id);
+        try{
+            if(zahvati.isEmpty()){
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            }
+            else
+                return new ResponseEntity<>(zahvati, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
