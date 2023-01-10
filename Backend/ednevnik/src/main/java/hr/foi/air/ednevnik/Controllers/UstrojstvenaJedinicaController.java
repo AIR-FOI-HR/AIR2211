c9ae60fb44858ae5efe8f647451f13a740f6f5c0
Roko <rokomilosevic@hotmail.com>
2023-01-10 15:16:52 +0100

    Dodana funkcija za dohvaćanje ustrojstvene jedinice preko ID-a.

---
+++ Backend/ednevnik/src/main/java/hr/foi/air/ednevnik/Controllers/UstrojstvenaJedinicaController.java
@@ -0,0 +1,34 @@
+package hr.foi.air.ednevnik.Controllers;
+
+
+import hr.foi.air.ednevnik.Entities.UstrojstvenaJedinica;
+import hr.foi.air.ednevnik.Services.UstrojstvenaJedinicaService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin
+@RequestMapping("/ustrojstvene_jedinice")
+public class UstrojstvenaJedinicaController {
+
+    private UstrojstvenaJedinicaService ustrojstvenaJedinicaService;
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<Optional<UstrojstvenaJedinica>> GetUstrojstvenaJedinicaById(@PathVariable int id){
+        var ustrojstvenaJedinica = ustrojstvenaJedinicaService.UstrojstvenaJedinicaById(id);
+        try{
+            if(ustrojstvenaJedinica.isEmpty()){
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            }
+            else
+                return new ResponseEntity<>(ustrojstvenaJedinica, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
