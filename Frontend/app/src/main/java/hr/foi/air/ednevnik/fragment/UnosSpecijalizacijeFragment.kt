aeaa898b05f4bd39219a8215d7d1c4de60f6aeac
Roko <rokomilosevic@hotmail.com>
2023-01-26 20:07:22 +0100

    Dodana mogućnost unosa specijalizacije za mentora.

--- Frontend/app/src/main/java/hr/foi/air/ednevnik/fragment/UnosSpecijalizacijeFragment.kt
+++ Frontend/app/src/main/java/hr/foi/air/ednevnik/fragment/UnosSpecijalizacijeFragment.kt
@@ -5,11 +5,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.core.entities.Specijalizacija
 import com.example.ws.WebServis
+import hr.foi.air.ednevnik.MainActivity
 import hr.foi.air.ednevnik.databinding.MentorUnosParametaraSpecijalizacijeBinding
 import java.text.SimpleDateFormat
 import java.util.*
@@ -21,7 +24,6 @@ class UnosSpecijalizacijeFragment : Fragment() {
         get() = _binding!!
 
     lateinit var webServis: WebServis
-    val listaSpecijalizacija = arrayOf("1", "2", "3")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,7 +36,9 @@ class UnosSpecijalizacijeFragment : Fragment() {
         _binding!!.gumbPotvrdiSpecijlizaciju.setOnClickListener {
             val formatter = SimpleDateFormat("yyyy-MM-dd")
             val current = formatter.format(Date())
-            var specijalizacija = Specijalizacija(null, args.argSpecijlizant.id_specijalizant, 1, 1, "test", current, null, null, "")
+            val programSpecijalizacije = _binding!!.inputProgramSpecijalizacije.editText?.text.toString()
+            val odobrenaZa = _binding!!.inputOdobrenoZa.editText?.text.toString()
+            var specijalizacija = Specijalizacija(null, args.argSpecijlizant.id_specijalizant, programSpecijalizacije.toInt(), MainActivity.mentor!!.idMentor, odobrenaZa, current, null, null, null)
             webServis.dodajSpecijalizaciju(specijalizacija) {
                 if(it == null){
                     Log.d("Tag", "Error pri dodavanju nove specke")
